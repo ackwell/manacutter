699a8c127ca39f3b2bdc114d35541cc9f2ba63ff
ackwell <saxon@ackwell.com.au>
2021-09-17 01:21:06 +1000

    Add rudimentary controller

---
+++ XIVAPI4/Controllers/SheetsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace XIVAPI4.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SheetsController : ControllerBase {
+	[HttpGet]
+	public Task<string> Get() {
+		var assemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+		if (assemblyLocation is null) { throw new Exception("Shit's fucked"); }
+		return System.IO.File.ReadAllTextAsync(Path.Combine(assemblyLocation, "Controllers", "temp-action-def.json"));
+	}
+}
