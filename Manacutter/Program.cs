23043729fd1bfc88f32d06d2e9624de924f6702f
ackwell <saxon@ackwell.com.au>
2021-09-24 23:00:37 +1000

    Move lumina registration to reader service

--- Manacutter/Program.cs
+++ Manacutter/Program.cs
@@ -2,10 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-// TODO: configurable data path
-var lumina = new Lumina.GameData("C:\\Program Files (x86)\\SquareEnix\\FINAL FANTASY XIV - A Realm Reborn\\game\\sqpack");
-builder.Services.AddSingleton(lumina);
-
 builder.Services
 	.AddGraphQL()
 	.AddReaders();
--- Manacutter/Services/Readers/Extensions.cs
+++ Manacutter/Services/Readers/Extensions.cs
@@ -1,10 +1,15 @@
-﻿using Manacutter.Services.Readers;
+﻿using Lumina;
+using Manacutter.Services.Readers;
 using Manacutter.Services.Readers.Lumina;
 
 namespace Microsoft.Extensions.DependencyInjection {
 	public static class ReaderServiceExtensions {
 		public static IServiceCollection AddReaders(this IServiceCollection services) {
-			// TODO: Register lumina here
+			// TODO: arguably the lumina reader should do this internally
+			// TODO: Configurable data location
+			var lumina = new GameData("C:\\Program Files (x86)\\SquareEnix\\FINAL FANTASY XIV - A Realm Reborn\\game\\sqpack");
+			services.AddSingleton(lumina);
+
 			services.AddSingleton<IReader, LuminaReader>();
 
 			return services;
