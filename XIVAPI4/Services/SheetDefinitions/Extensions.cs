ef201f08445ae78df40061ccc3b4e50ccfc4c6e9
ackwell <saxon@ackwell.com.au>
2021-09-24 20:34:26 +1000

    Clean up old definitions extensions

---
+++ XIVAPI4/Services/SheetDefinitions/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using XIVAPI4.Services.SheetDefinitions;
+using XIVAPI4.Services.SheetDefinitions.SaintCoinach;
+
+namespace Microsoft.Extensions.DependencyInjection {
+	public static class SheetDefinitionServiceExtensions {
+		public static IServiceCollection AddSheetDefinitionProviders(this IServiceCollection services, IConfiguration configuration) {
+			services.Configure<SaintCoinachOptions>(configuration.GetSection(SaintCoinachOptions.Name));
+			services.AddSingleton<ISheetDefinitionProvider, SaintCoinachProvider>();
+
+			services.AddHostedService<SheetDefinitionHostedService>();
+
+			return services;
+		}
+	}
+}
+
+namespace XIVAPI4.Services.SheetDefinitions {
+	internal class SheetDefinitionHostedService : IHostedService {
+		private readonly IServiceProvider serviceProvider;
+		public SheetDefinitionHostedService(IServiceProvider serviceProvider) {
+			this.serviceProvider = serviceProvider;
+		}
+
+		public Task StartAsync(CancellationToken cancellationToken) {
+			var providers = this.serviceProvider.GetRequiredService<IEnumerable<ISheetDefinitionProvider>>();
+			return Task.WhenAll(providers.Select(provider => provider.Initialize()));
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+	}
+}
