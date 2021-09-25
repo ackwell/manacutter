49427d5f807131d877fc438f4120630143ce5d6e
ackwell <saxon@ackwell.com.au>
2021-09-25 17:40:14 +1000

    Fill out StC schema a bit

--- Manacutter/Services/Definitions/SaintCoinach/SheetDefinition.cs
+++ Manacutter/Services/Definitions/SaintCoinach/SheetDefinition.cs
@@ -2,12 +2,24 @@
 
 #pragma warning disable CS8618
 
+// TODO: StC has classes for all this shit, but it desers a JObject and builds manually. Possibly the "best" long-term solution, look into viability.
+
 public class SheetDefinition {
-	public List<ColumnDefinition> Definitions { get; set; }
+	public List<DefinitionEntry> Definitions { get; set; }
 }
 
-// TODO: rename?
-public class ColumnDefinition {
-	public int Index { get; set; } = 0;
+public class DefinitionEntry {
+	public uint Index { get; set; } = 0;
 	public string? Name { get; set; }
+	public string? Type { get; set; }
+	public uint? Count { get; set; }
+	public DefinitionEntry? Definition { get; set; }
+	public List<DefinitionEntry>? Members { get; set; }
+	public Converter? Converter { get; set; }
+}
+
+public class Converter {
+	public string Type { get; set; }
+	public string? Target { get; set; }
+	// TODO: Fields like "links" for complexlink, etc
 }
