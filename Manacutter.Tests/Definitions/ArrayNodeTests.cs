6de076804a81915d1002775bf03f5777bb9b590c
ackwell <saxon@ackwell.com.au>
2021-09-28 20:56:32 +1000

    Add some trivial tests for array / struct size

---
+++ Manacutter.Tests/Definitions/ArrayNodeTests.cs
@@ -0,0 +1,16 @@
+﻿using Manacutter.Definitions;
+using Xunit;
+
+namespace Manacutter.Tests.Definitions;
+
+public class ArrayNodeTests {
+	[Theory]
+	[InlineData(1)]
+	[InlineData(5)]
+	public void CountSize(uint count) {
+		var scalarNode = new ScalarNode();
+		var arrayNode = new ArrayNode(scalarNode, count);
+
+		Assert.Equal(count, arrayNode.Size);
+	}
+}
---
+++ Manacutter.Tests/Definitions/StructNodeTests.cs
@@ -0,0 +1,23 @@
+﻿using Manacutter.Definitions;
+using System.Linq;
+using Xunit;
+
+namespace Manacutter.Tests.Definitions;
+
+public class StructNodeTests {
+	[Theory]
+	[InlineData(1)]
+	[InlineData(5)]
+	public void CountSize(uint count) {
+		var fields = Enumerable
+			.Range(0, (int)count)
+			.ToDictionary(
+				value => value.ToString(),
+				_ => new ScalarNode() as DefinitionNode
+			);
+
+		var structNode = new StructNode(fields);
+
+		Assert.Equal(count, structNode.Size);
+	}
+}
