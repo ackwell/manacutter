570a628ccb0589c8288a412fde7c3cbb5bf54303
ackwell <saxon@ackwell.com.au>
2021-10-15 23:35:15 +1100

    Move LCS to own file

---
+++ Manacutter.Definitions/SaintCoinach/Helpers.cs
@@ -0,0 +1,41 @@
+﻿namespace Manacutter.Definitions.SaintCoinach;
+
+internal class Helpers {
+	// TODO: Check if we need to look into optimisations e.g. suffix tree
+	// Thanks, wikipedia
+	internal static string LongestCommonSubsequence(string a, string b) {
+		// Initalise table
+		int[,] table = new int[a.Length + 1, b.Length + 1];
+
+		// LCS algo
+		int i;
+		int j;
+		for (i = 1; i <= a.Length; i++) {
+			for (j = 1; j <= b.Length; j++) {
+				table[i, j] = a[i - 1] == b[j - 1]
+					? table[i, j] = table[i - 1, j - 1] + 1
+					: table[i - 1, j] > table[i, j - 1]
+						? table[i - 1, j]
+						: table[i, j - 1];
+			}
+		}
+
+		// Backtrack the table into a string
+		var output = "";
+		i = a.Length;
+		j = b.Length;
+		while (i > 0 && j > 0) {
+			if (a[i - 1] == b[j - 1]) {
+				output = a[i - 1] + output;
+				i--;
+				j--;
+			} else if (table[i - 1, j] > table[i, j - 1]) {
+				i--;
+			} else {
+				j--;
+			}
+		}
+
+		return output;
+	}
+}
--- Manacutter.Definitions/SaintCoinach/SaintCoinachProvider.cs
+++ Manacutter.Definitions/SaintCoinach/SaintCoinachProvider.cs
@@ -194,7 +194,7 @@ internal class SaintCoinachProvider : IDefinitionProvider, IDisposable {
 		}
 
 		var node = new StructNode(fields);
-		var lcs = fields.Keys.Aggregate(GetLCS);
+		var lcs = fields.Keys.Aggregate(Helpers.LongestCommonSubsequence);
 		var name = lcs != "" ? lcs : null;
 
 		return (new StructNode(fields), name);
@@ -245,44 +245,5 @@ internal class SaintCoinachProvider : IDefinitionProvider, IDisposable {
 		// Yikes.
 		// https://github.com/xivapi/SaintCoinach/blob/111543bf399c709529237cfb25f77650ddb0126f/SaintCoinach/Ex/Relational/ValueConverters/ComplexLinkConverter.cs#L143
 		return new ScalarNode();
-	}
-
-	// TODO: Move this somewhere more sensible lmao
-	// TODO: Check if we need to look into optimisations e.g. suffix tree
-	// Thanks, wikipedia
-	private static string GetLCS(string a, string b) {
-		// Initalise table
-		int[,] table = new int[a.Length + 1, b.Length + 1];
-
-		// LCS algo
-		int i;
-		int j;
-		for (i = 1; i <= a.Length; i++) {
-			for (j = 1; j <= b.Length; j++) {
-				table[i, j] = a[i - 1] == b[j - 1]
-					? table[i, j] = table[i - 1, j - 1] + 1
-					: table[i - 1, j] > table[i, j - 1]
-						? table[i - 1, j]
-						: table[i, j - 1];
-			}
-		}
-
-		// Backtrack the table into a string
-		var output = "";
-		i = a.Length;
-		j = b.Length;
-		while (i > 0 && j > 0) {
-			if (a[i - 1] == b[j - 1]) {
-				output = a[i - 1] + output;
-				i--;
-				j--;
-			} else if (table[i - 1, j] > table[i, j - 1]) {
-				i--;
-			} else {
-				j--;
-			}
-		}
-
-		return output;
 	}
 }
