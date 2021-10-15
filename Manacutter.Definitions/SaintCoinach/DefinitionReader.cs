5be22b545b240230e897eec86c407280b4a626fe
ackwell <saxon@ackwell.com.au>
2021-10-16 00:10:44 +1100

    Add references to CS impl

--- Manacutter.Definitions/SaintCoinach/DefinitionReader.cs
+++ Manacutter.Definitions/SaintCoinach/DefinitionReader.cs
@@ -1,9 +1,10 @@
-using Manacutter.Common.Schema;
+﻿using Manacutter.Common.Schema;
 using System.Text.Json;
 
 namespace Manacutter.Definitions.SaintCoinach;
 
 internal static class DefinitionReader {
+	/// <seealso href="https://github.com/xivapi/SaintCoinach/blob/800eab3e9dd4a2abc625f53ce84dad24c8579920/SaintCoinach/Ex/Relational/Definition/SheetDefinition.cs#L157">SheetDefinition.cs#L157</seealso>
 	internal static SchemaNode ReadSheetDefinition(in JsonElement element) {
 		var fields = new Dictionary<string, SchemaNode>();
 
@@ -16,6 +17,7 @@ internal static class DefinitionReader {
 		return new StructNode(fields);
 	}
 
+	/// <seealso href="https://github.com/xivapi/SaintCoinach/blob/800eab3e9dd4a2abc625f53ce84dad24c8579920/SaintCoinach/Ex/Relational/Definition/PositionedDataDefinition.cs#L71">PositionedDataDefinition.cs#L71</seealso>
 	private static (SchemaNode, string?) ReadPositionedDataDefinition(in JsonElement element) {
 		var index = element.TryGetProperty("index", out var property)
 			? property.GetUInt32()
@@ -25,6 +27,7 @@ internal static class DefinitionReader {
 		return (node with { Offset = index }, name);
 	}
 
+	/// <seealso href="https://github.com/xivapi/SaintCoinach/blob/800eab3e9dd4a2abc625f53ce84dad24c8579920/SaintCoinach/Ex/Relational/Definition/IDataDefinition.cs#L34">IDataDefinition.cs#L34</seealso>
 	private static (SchemaNode, string?) ReadDataDefinition(in JsonElement element) {
 		var type = element.TryGetProperty("type", out var property)
 			? property.GetString()
@@ -38,6 +41,7 @@ internal static class DefinitionReader {
 		};
 	}
 
+	/// <seealso href="https://github.com/xivapi/SaintCoinach/blob/800eab3e9dd4a2abc625f53ce84dad24c8579920/SaintCoinach/Ex/Relational/Definition/SingleDataDefinition.cs#L66">SingleDataDefinition.cs#L66</seealso>
 	private static (SchemaNode, string?) ReadSingleDataDefinition(in JsonElement element) {
 		var name = element.GetProperty("name").GetString();
 		var converterExists = element.TryGetProperty("converter", out var converter);
@@ -50,6 +54,7 @@ internal static class DefinitionReader {
 			? property.GetString()
 			: null;
 
+		// TODO: There's also a "quad" type with a converter but I've got no idea how it's instantiated.
 		var node = type switch {
 			"color" => ReadColorConverter(converter),
 			"generic" => ReadGenericReferenceConverter(converter),
@@ -64,6 +69,7 @@ internal static class DefinitionReader {
 		return (node, name);
 	}
 
+	/// <seealso href="https://github.com/xivapi/SaintCoinach/blob/800eab3e9dd4a2abc625f53ce84dad24c8579920/SaintCoinach/Ex/Relational/Definition/GroupDataDefinition.cs#L125">GroupDataDefinition.cs#L125</seealso>
 	private static (SchemaNode, string?) ReadGroupDataDefinition(in JsonElement element) {
 		var fields = new Dictionary<string, SchemaNode>();
 
@@ -85,6 +91,7 @@ internal static class DefinitionReader {
 		return (new StructNode(fields), name);
 	}
 
+	/// <seealso href="https://github.com/xivapi/SaintCoinach/blob/800eab3e9dd4a2abc625f53ce84dad24c8579920/SaintCoinach/Ex/Relational/Definition/RepeatDataDefinition.cs#L85">RepeatDataDefinition.cs#L85</seealso>
 	private static (SchemaNode, string?) ReadRepeatDataDefinition(in JsonElement element) {
 		var (childNode, childName) = ReadDataDefinition(element.GetProperty("definition"));
 		return (
@@ -93,38 +100,45 @@ internal static class DefinitionReader {
 		);
 	}
 
+	/// <seealso href="https://github.com/xivapi/SaintCoinach/blob/800eab3e9dd4a2abc625f53ce84dad24c8579920/SaintCoinach/Ex/Relational/ValueConverters/ColorConverter.cs#L46">ColorConverter.cs#L46</seealso>
 	private static SchemaNode ReadColorConverter(in JsonElement element) {
 		// TODO: ?
 		return new ScalarNode();
 	}
 
+	/// <seealso href="https://github.com/xivapi/SaintCoinach/blob/800eab3e9dd4a2abc625f53ce84dad24c8579920/SaintCoinach/Ex/Relational/ValueConverters/GenericReferenceConverter.cs#L33">GenericReferenceConverter.cs#L33</seealso>
 	private static SchemaNode ReadGenericReferenceConverter(in JsonElement element) {
 		// TODO: ?
 		return new ScalarNode();
 	}
 
+	/// <seealso href="https://github.com/xivapi/SaintCoinach/blob/800eab3e9dd4a2abc625f53ce84dad24c8579920/SaintCoinach/Ex/Relational/ValueConverters/IconConverter.cs#L33">IconConverter.cs#L33</seealso>
 	private static SchemaNode ReadIconConverter(in JsonElement element) {
 		// TODO: ?
 		return new ScalarNode();
 	}
 
+	/// <seealso href="https://github.com/xivapi/SaintCoinach/blob/800eab3e9dd4a2abc625f53ce84dad24c8579920/SaintCoinach/Ex/Relational/ValueConverters/MultiReferenceConverter.cs#L50">MultiReferenceConverter.cs#L50</seealso>
 	private static SchemaNode ReadMultiReferenceConverter(in JsonElement element) {
 		// TODO: Reference node
 		// element["targets"] = array of target sheet names
 		return new ScalarNode();
 	}
 
+	/// <seealso href="https://github.com/xivapi/SaintCoinach/blob/800eab3e9dd4a2abc625f53ce84dad24c8579920/SaintCoinach/Ex/Relational/ValueConverters/SheetLinkConverter.cs#L40">SheetLinkConverter.cs#L40</seealso>
 	private static SchemaNode ReadSheetLinkConverter(in JsonElement element) {
 		// TODO: Reference node
 		// element["target"] = target sheet name
 		return new ScalarNode();
 	}
 
+	/// <seealso href="https://github.com/xivapi/SaintCoinach/blob/800eab3e9dd4a2abc625f53ce84dad24c8579920/SaintCoinach/Ex/Relational/ValueConverters/TomestoneOrItemReferenceConverter.cs#L54">TomestoneOrItemReferenceConverter.cs#L54</seealso>
 	private static SchemaNode ReadTomestoneOrItemReferenceConverter(in JsonElement element) {
 		// TODO: ?
 		return new ScalarNode();
 	}
 
+	/// <seealso href="https://github.com/xivapi/SaintCoinach/blob/800eab3e9dd4a2abc625f53ce84dad24c8579920/SaintCoinach/Ex/Relational/ValueConverters/ComplexLinkConverter.cs#L143">ComplexLinkConverter.cs#L143</seealso>
 	private static SchemaNode ReadComplexLinkConverter(in JsonElement element) {
 		// TODO: Reference node.
 		return new ScalarNode();
