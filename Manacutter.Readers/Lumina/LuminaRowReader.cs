3357d399182732c471ecefb0b73a0fa783f5f881
ackwell <saxon@ackwell.com.au>
2021-12-27 19:42:08 +1100

    Package updates

--- Manacutter.Readers/Lumina/LuminaRowReader.cs
+++ Manacutter.Readers/Lumina/LuminaRowReader.cs
@@ -12,8 +12,8 @@ internal class LuminaRowReader : IRowReader {
 		this.rowParser = rowParser;
 	}
 
-	public uint RowID => this.rowParser.Row;
-	public uint SubRowID => this.rowParser.SubRow;
+	public uint RowID => this.rowParser.RowId;
+	public uint SubRowID => this.rowParser.SubRowId;
 
 	public object ReadColumn(uint columnIndex) {
 		var value = this.rowParser.ReadColumnRaw((int)columnIndex);
@@ -22,7 +22,11 @@ internal class LuminaRowReader : IRowReader {
 
 		// TODO: Will probably need slightly more involved logic for SeString in the long run.
 		if (column.Type == ExcelColumnDataType.String) {
-			value = value.ToString()!;
+			value = value?.ToString();
+		}
+
+		if (value is null) {
+			throw new ArgumentOutOfRangeException();
 		}
 
 		return value;
